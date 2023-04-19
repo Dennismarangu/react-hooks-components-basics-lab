756d037fee8fad1d29a397ce88a62d7d713659ea
Dennismarangu <dennismutumamarangu@gmail.com>
2023-04-19 11:53:48 +0300

    Completed assignment

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavBar() {
+    return (
+      <nav>
+        <a href="#home">I'm a link!</a>
+      </nav>
+    );
+  }
+
+  // to enable reusage of Navbar
+  export default NavBar;
